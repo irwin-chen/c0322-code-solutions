712e6d02fbadfdea343f79a044b49e147d0b96af
Irwin Chen <irwin.t.chen@gmail.com>
2022-03-21 19:35:06 +0000

    Completed javascript-operators-and-expressions

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,47 @@
+var width = 7;
+var height = 20;
+
+var area = width * height;
+
+console.log('Area:', area);
+console.log('typeof area:', typeof area);
+
+var bill = 100;
+var payment = 300;
+
+var change = payment - bill;
+console.log('Change:', change);
+console.log('typeof change:', typeof change);
+
+var quizzes = 75;
+var midterms = 85;
+var final = 90;
+
+var grade = (quizzes + midterms + final) / 3;
+console.log('Grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+var firstName = 'Irwin';
+var lastName = 'Chen';
+
+var fullName = firstName + ' ' + lastName;
+console.log('Full Name:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+var pH = 8;
+
+var isAcidic = pH < 7;
+console.log('IsAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+var headCount = 290;
+
+var isSparta = headCount === 300;
+console.log('IsSparta:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+var motto = fullName;
+
+motto += ' is the GOAT';
+console.log('Motto:', motto);
+console.log('typeof motto:', typeof motto);
