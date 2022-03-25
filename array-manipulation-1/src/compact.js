70e919fd52516f641e194f14b1a8bdea51a9c424
Irwin Chen <irwin.t.chen@gmail.com>
2022-03-25 23:40:15 +0000

    Completed array-manipulation-1 exercise.

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -1,1 +1,11 @@
 /* exported compact */
+
+function compact(array) {
+  var compact = [];
+  for (var i = 0; i < array.length; i++) {
+    if (array[i]) {
+      compact.push(array[i]);
+    }
+  }
+  return compact;
+}
