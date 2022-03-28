904d8abb17893c076e8eafb01fa433e1379f57e7
Irwin Chen <irwin.t.chen@gmail.com>
2022-03-28 21:57:50 +0000

    Completed array-manipulation-2

--- array-manipulation-2/src/take.js
+++ array-manipulation-2/src/take.js
@@ -10,3 +10,12 @@ function take(array, count) {
   }
   return newArray;
 }
+
+// take (array, count)
+// takes an array and argument count, takes the first number of elements specified in the count and returns them in a new array
+// - create a new array
+// - check to see if the array length is 0
+//   - if so, return the empty array
+// - for each index until the specified count
+//   - push the element at the correpsonding index in the argument array into the new array
+// - return the new array from the function
