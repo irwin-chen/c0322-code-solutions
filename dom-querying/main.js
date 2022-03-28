cb13e23d4100b4f6f0aeea2dedd2b9a6e8bc11c5
Irwin Chen <irwin.t.chen@gmail.com>
2022-03-28 17:31:27 +0000

    Completed dom-querying exercise.

---
+++ dom-querying/main.js
@@ -0,0 +1,13 @@
+console.log('hello, world');
+
+console.log('heading:', document.querySelector('h1'));
+console.dir(document.querySelector('h1'));
+
+console.log('$id:', document.querySelector('#explanation'));
+console.dir(document.querySelector('#explanation'));
+
+console.log('$class:', document.querySelector('.hint'));
+console.dir(document.querySelector('.hint'));
+
+console.log('$element all:', document.querySelectorAll('p'));
+console.log('$class all:', document.querySelectorAll('.example-link'));
