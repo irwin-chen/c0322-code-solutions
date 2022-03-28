a5685e20f6aeccec1de6cbd774806d7e877fb988
Irwin Chen <irwin.t.chen@gmail.com>
2022-03-28 18:15:45 +0000

    in progress

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,1 +1,13 @@
 /* exported chunk */
+
+function chunk(array, size) {
+
+}
+
+// chunk(array, size)
+// takes in an argument array and splits it into sub arrays with array lengths specified by the argument size. Any leftover elements are placed into a final array
+// - create new array, holds the entire result array
+// - check for remainders
+//   - if remainders
+//     - for each remainder
+//       - push it into a subarray for all the remainders
--- array-manipulation-2/src/drop-right.js
+++ array-manipulation-2/src/drop-right.js
@@ -1,1 +1,14 @@
 /* exported dropRight */
+function dropRight(array, count) {
+  var result = [];
+  for (var i = 0; i < array.length - count; i++) {
+    result.push(array[i]);
+  }
+  return result;
+}
+
+// dropRight(array, count)
+// takes in an argument array, returns a new array without the number of elements specified by the argument count, starting from the end
+// - create a new array
+// - for each value before cutting off at the number specified, but starting from teh beginning of the index
+//   - push the value into the result array
