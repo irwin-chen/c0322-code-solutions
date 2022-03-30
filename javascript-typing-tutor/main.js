4813470acf26201c31b8f6926169f1e645d1aaca
Irwin Chen <irwin.t.chen@gmail.com>
2022-03-30 21:36:25 +0000

    in progress

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,4 +1,9 @@
 var $text = document.querySelectorAll('span');
+var $button = document.querySelector('button');
+var $span = document.querySelectorAll('span');
+var $accuracy = document.querySelector('.accuracy-text');
+var wordBank = ['tudor', 'trial', 'error', 'css', 'JavaScript', 'kappa', 'smadge', 'template', 'correct', 'hidden', 'text', 'align', 'justify', 'height'];
+
 var counter = 0;
 var check = null;
 var wrong = 0;
@@ -15,8 +20,23 @@ document.addEventListener('keydown', function (event) {
 
   if (counter === $text.length) {
     checkAccuracy();
-    document.querySelector('button').classList.remove = 'hidden';
+    $button.classList.remove('hidden');
   }
+});
+
+$button.addEventListener('click', function () {
+  $span[0].className = 'current-letter';
+  for (var i = 1; i < $text.length; i++) {
+    $span[i].classList.remove('correct');
+  }
+  counter = 0;
+  check = null;
+  wrong = 0;
+  keystroke = 0;
+  $button.classList.add('hidden');
+  $accuracy.classList.add('hidden');
+  // word randomiser
+  scramble();
 });
 
 function rightOrWrong() {
@@ -34,8 +54,23 @@ function rightOrWrong() {
 }
 
 function checkAccuracy() {
-  var $accuracy = document.querySelector('.accuracy-text');
   var accuracy = Math.round((1 - (wrong / keystroke)) * 100);
   $accuracy.textContent = 'Accuracy: ' + accuracy + '%';
-  $accuracy.getClass = 'accuracy-text';
+  $accuracy.classList.remove('hidden');
+}
+
+function scramble() {
+  var string = wordBank[Math.trunc(Math.random() * wordBank.length)];
+  for (var i = 1; i < 5; i++) {
+    string += ' ' + wordBank[Math.trunc(Math.random() * wordBank.length)];
+  }
+  for (var x = 0; x < $text.length; x++) {
+    $text[x].remove();
+  }
+
+  for (var y = 0; y < string.length; y++) {
+    var $temp = document.createElement('span');
+    $temp.textContent = string[y];
+    document.querySelector('p').appendChild($temp);
+  }
 }
