649921992261158d829f943f63201b658d6e93ee
Irwin Chen <irwin.t.chen@gmail.com>
2022-03-30 18:37:10 +0000

    simplified the js

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,25 +1,11 @@
 var $modal = document.querySelector('.modal-button');
 var $no = document.querySelector('.no-button');
 var $popUp = document.querySelector('.pop-up');
-var toggle = null;
 
 $modal.addEventListener('click', function (event) {
-  toggle = true;
-  toggleOnOff();
+  $popUp.className = 'pop-up';
 });
 
 $no.addEventListener('click', function (event) {
-  toggle = false;
-  toggleOnOff();
+  $popUp.className = 'pop-up hidden';
 });
-
-function toggleOnOff() {
-  var x;
-  if (toggle) {
-    x = '';
-  }
-  if (!toggle) {
-    x = ' hidden';
-  }
-  $popUp.className = 'pop-up' + x;
-}
