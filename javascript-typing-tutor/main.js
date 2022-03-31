35480fc927d9e5808f303b8e7dd584f5117bb1ee
Irwin Chen <irwin.t.chen@gmail.com>
2022-03-31 01:44:23 +0000

    typing tutor with word randomizer, ran event paramater to run through rightOrWrong function to reduce line count

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,22 +1,16 @@
 var $text = document.querySelectorAll('span');
 var $button = document.querySelector('button');
-var $span = document.querySelectorAll('span');
 var $accuracy = document.querySelector('.accuracy-text');
-// var wordBank = ['tudor', 'trial', 'error', 'css', 'JavaScript', 'kappa', 'smadge', 'template', 'correct', 'hidden', 'text', 'align', 'justify', 'height'];
+var $p = document.querySelector('p');
+var wordBank = ['tudor', 'trial', 'error', 'css', 'JavaScript', 'kappa', 'smadge', 'template', 'correct', 'hidden', 'text', 'align', 'justify', 'height'];
 
 var counter = 0;
-var check = null;
 var wrong = 0;
 var keystroke = 0;
 
 document.addEventListener('keydown', function (event) {
   keystroke++;
-  if (event.key === $text[counter].textContent) {
-    check = true;
-  } else {
-    check = false;
-  }
-  rightOrWrong();
+  rightOrWrong(event);
 
   if (counter === $text.length) {
     checkAccuracy();
@@ -25,29 +19,27 @@ document.addEventListener('keydown', function (event) {
 });
 
 $button.addEventListener('click', function () {
-  $span[0].className = 'current-letter';
+  $text[0].className = 'current-letter';
   for (var i = 1; i < $text.length; i++) {
-    $span[i].classList.remove('correct');
+    $text[i].classList.remove('correct');
   }
   counter = 0;
-  check = null;
   wrong = 0;
   keystroke = 0;
   $button.classList.add('hidden');
   $accuracy.classList.add('hidden');
   // word randomiser
-  // scramble();
+  scramble();
 });
 
-function rightOrWrong() {
-  if (check) {
+function rightOrWrong(event) {
+  if (event.key === $text[counter].textContent) {
     $text[counter].className = 'correct';
     counter++;
     if (counter < $text.length) {
       $text[counter].className = 'current-letter';
     }
-
-  } else if (!check) {
+  } else {
     $text[counter].className = 'incorrect current-letter';
     wrong++;
   }
@@ -59,18 +51,20 @@ function checkAccuracy() {
   $accuracy.classList.remove('hidden');
 }
 
-// function scramble() {
-//   var string = wordBank[Math.trunc(Math.random() * wordBank.length)];
-//   for (var i = 1; i < 5; i++) {
-//     string += ' ' + wordBank[Math.trunc(Math.random() * wordBank.length)];
-//   }
-//   for (var x = 0; x < $text.length; x++) {
-//     $text[x].remove();
-//   }
+function scramble() {
+  var string = wordBank[Math.trunc(Math.random() * wordBank.length)];
+  for (var i = 1; i < 5; i++) {
+    string += ' ' + wordBank[Math.trunc(Math.random() * wordBank.length)];
+  }
+  for (var x = 0; x < $text.length; x++) {
+    $text[x].remove();
+  }
 
-//   for (var y = 0; y < string.length; y++) {
-//     var $temp = document.createElement('span');
-//     $temp.textContent = string[y];
-//     document.querySelector('p').appendChild($temp);
-//   }
-// }
+  for (var y = 0; y < string.length; y++) {
+    var $temp = document.createElement('span');
+    $temp.textContent = string[y];
+    $p.appendChild($temp);
+  }
+  $text = document.querySelectorAll('span');
+  document.querySelector('span').className = 'current-letter';
+}
