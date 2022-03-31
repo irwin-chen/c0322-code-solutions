f664b74ee7659c7d2025fa4d95fe32d6836cea3b
Irwin Chen <irwin.t.chen@gmail.com>
2022-03-31 22:46:05 +0000

    in progress

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,12 +1,15 @@
 /* exported pick */
 
 function pick(source, keys) {
+  // debugger;
   var results = {};
-  for (var i = 0; i < keys.length; i++) {
-    var tempArray = Object.keys(source);
-    for (var x = 0; x < tempArray.length; x++) {
-      if (tempArray[x] === keys[i]) {
-        results[keys[i]] = source[keys[i]];
+  var objArray = Object.keys(source);
+  for (var i = 0; i < objArray.length; i++) {
+    for (var x = 0; x < keys.length; x++) {
+      if (objArray[i] === keys[x]) {
+        if (source[keys[x]] !== undefined) {
+          results[keys[x]] = source[keys[x]];
+        }
       }
     }
   }
@@ -14,9 +17,12 @@ function pick(source, keys) {
 }
 
 // pick(source, keys)
-// creates an object with the key properties specified in the keys array from a source object
-// - create empty object to hold the results
-//   - for each key property in the array
-//     - find the corresponding value inside the object
-//     - create the same property inside the new object, assign the value from the source object
-// - return the results of the new object from the function
+// takes a js object and array as an argument, based on the array of keys (string values) provided, if the keys are found
+// in the argument object, it takes that corresponding key and value and places them into a new object
+// - create new object to hold the results
+// - create a new array to hold the keys for the
+//   - for each key in the sourc
+//   - check if the array key exists in the argument source object
+//     - check if the value at the key is undefined
+//       - assign the value of that key to the same key property in the new object
+// - return new object from the function
