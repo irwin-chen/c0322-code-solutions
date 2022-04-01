37a0ae2a31bf21e9d9e95511c047faf928517398
Irwin Chen <irwin.t.chen@gmail.com>
2022-04-01 20:30:16 +0000

    made some changes to the defaults function pseudocode

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -24,6 +24,8 @@ function defaults(target, source) {
 // compares properties of both the target and source argument objects, any missing properties from the target object and pulled from the source object
 // - create array of properties from the target object
 // - create an array of properties from the source object
+// - check if target array length is 0
+//   - if so, copy over the source object to the target object
 //   - for each index of the source array
 //     - create a variable counter to check how many times the word is not in the target array index
 //     - for each index of the target array
