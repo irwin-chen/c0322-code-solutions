dc672353260af2f0c50dd29af8aa134f221daac3
Irwin Chen <irwin.t.chen@gmail.com>
2022-04-12 00:48:22 +0000

    made changes to pseudocode

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -20,14 +20,12 @@ function defaults(target, source) {
 
 // defaults(target, source)
 // compares properties of both the target and source argument objects, any missing properties from the target object and pulled from the source object
-// - create array of properties from the target object
-// - create an array of properties from the source object
-// - check if target array length is 0
-//   - if so, copy over the source object to the target object
-//   - for each index of the source array
-//     - create a variable counter to check how many times the word is not in the target array index
-//     - for each index of the target array
-//       - compare the source property at that index to see if they can be found in the target array
-//         - if not, increase the counter
-//           - if the target array loop finishes without matching a single word
-//             - the property and property value from the source is pushed into the target object
+// - check if the object is empty
+//   - if so, copy over the source object properties into the target properties
+//   - otherwise, for every property in the source object
+//     - initialize a counter to 0
+//     - for each property in the target object
+//       - check if the target property and the source property match URLSearchParams
+//         - if they dont, increase teh counter by one
+//         - if the counter is equal to the number of entries in the target object
+//           - push the "unused" property into the target object and give it its corresponding value
