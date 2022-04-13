28a966870c7e122156a2309fc20533593e64b1da
Irwin Chen <irwin.t.chen@gmail.com>
2022-04-13 17:07:57 +0000

    in prog

---
+++ object-manipulation-3/main.js
@@ -0,0 +1,27 @@
+console.log('Lodash is loaded:', typeof _ !== 'undefined');
+
+var deck = [
+  {
+    rank: ['a', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'j', 'q', 'k'],
+    suit: ['c', 'd', 'h', 's']
+  }
+];
+
+var players = [
+  {
+    name: 'Jimmy',
+    hand: []
+  },
+  {
+    name: 'Jamie',
+    hand: []
+  },
+  {
+    name: 'Jango',
+    hand: []
+  },
+  {
+    name: 'Jorothy',
+    hand: []
+  }
+];
