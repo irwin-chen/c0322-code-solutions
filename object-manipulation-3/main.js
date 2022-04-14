7ae691eba3bb9734f764b896994864d6ef152810
Irwin Chen <irwin.t.chen@gmail.com>
2022-04-14 06:30:30 +0000

    Completed object-manipulation-3 exercise.

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -3,6 +3,7 @@ console.log('Lodash is loaded:', typeof _ !== 'undefined');
 var deck = [];
 var rank = ['ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king'];
 var suit = ['clubs', 'diamonds', 'hearts', 'spades'];
+var points = [];
 
 var players = [
   {
@@ -35,10 +36,50 @@ function createDeck() {
       deck.push(newCard);
     }
   }
-  console.log(deck);
+  deck = _.shuffle(deck);
 }
 
 function deal() {
-  _.shuffle(deck);
-  console.log('?');
+  for (var i = 0; i < players.length; i++) {
+    while (players[i].hand.length < 2) {
+      players[i].hand.push(deck.shift());
+    }
+  }
 }
+
+function checkValues() {
+  for (var i = 0; i < players.length; i++) {
+    var sum = 0;
+    for (var j = 0; j < players[i].hand.length; j++) {
+      var holder = players[i].hand[j].rank;
+      if (holder === 'ace') {
+        sum += 11;
+      } else if (holder === 'jack' || holder === 'queen' || holder === 'king') {
+        sum += 10;
+      } else {
+        sum += holder;
+      }
+    }
+    points.push(sum);
+  }
+}
+
+function winner() {
+  var max = _.max(points);
+  var index = points.indexOf(max);
+  var lastIndex = points.lastIndexOf(max);
+  if (lastIndex !== index) {
+    console.log('There\'s a tie');
+  } else {
+    console.log('the winner is ' + players[index].name + ' with ' + max + ' points!');
+  }
+}
+
+function play() {
+  createDeck();
+  deal();
+  checkValues();
+  winner();
+}
+
+play();
