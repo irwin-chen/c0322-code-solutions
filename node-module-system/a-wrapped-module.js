6de8ca2ff506fb3bf27b993e6d0ca0c44f3eceb1
Irwin Chen <irwin.t.chen@gmail.com>
2022-05-03 23:53:55 +0000

    Completed node-module-system exercise.

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,14 @@
+console.log('Typeof Exports: ', typeof exports);
+console.log('Value of Exports: ', exports);
+
+console.log('Typeof Require: ', typeof require);
+console.log('Value of Require: ', require);
+
+console.log('Typeof Module: ', typeof module);
+console.log('Value of Module: ', module);
+
+console.log('Typeof __filename: ', typeof __filename);
+console.log('Value of __filename: ', __filename);
+
+console.log('Typeof __dirname: ', typeof __dirname);
+console.log('Value of __dirname: ', __dirname);
