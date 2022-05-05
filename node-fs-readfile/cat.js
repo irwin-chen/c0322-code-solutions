1578da3a6933acf617ae3d85f0c84f4277e2bbfa
Irwin Chen <irwin.t.chen@gmail.com>
2022-05-05 18:12:40 +0000

    level 3 in prog

--- node-fs-readfile/cat.js
+++ node-fs-readfile/cat.js
@@ -2,16 +2,13 @@ const fs = require('fs');
 const [, , ...rest] = process.argv;
 
 console.log(rest);
-let counter = 0;
 
+const counter = 0;
 for (let i = 0; i < rest.length; i++) {
   fs.readFile(rest[i], 'utf8', (err, data) => {
     if (err) {
       throw err;
     }
-    while (counter === i) {
-      console.log(data);
-      counter++;
-    }
+    console.log(counter, data);
   });
 }
