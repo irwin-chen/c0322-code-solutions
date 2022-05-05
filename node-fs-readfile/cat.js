067081d2ecbd4c3cb11d61a2de40111abb25c5cf
Irwin Chen <irwin.t.chen@gmail.com>
2022-05-05 19:58:33 +0000

    removed extra arr, removed for loop for arr

--- node-fs-readfile/cat.js
+++ node-fs-readfile/cat.js
@@ -2,22 +2,16 @@ const fs = require('fs');
 const [, , ...rest] = process.argv;
 
 let counter = 0;
-const arr = [];
 
 function recursion() {
   fs.readFile(rest[counter], 'utf8', (err, data) => {
     if (err) {
       throw err;
     }
-    arr.push(data);
     counter++;
-
+    console.log(data);
     if (counter < rest.length) {
       recursion();
-    } else if (counter === rest.length) {
-      for (let i = 0; i < rest.length; i++) {
-        console.log(arr[i]);
-      }
     }
   });
 }
