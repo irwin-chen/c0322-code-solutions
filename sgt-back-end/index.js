df350f68748b6d952413acc2be48978ebf96efee
Irwin Chen <irwin.t.chen@gmail.com>
2022-05-13 18:44:08 +0000

    worked on put

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -27,21 +27,17 @@ app.get('/api/grades', (req, res) => {
 });
 
 app.use(express.json());
-app.post('/api/grades', (req, res) => {
-  const gradeEntry = req.body;
-  const entryValues = [];
-  for (const prop in gradeEntry) {
-    entryValues.push(gradeEntry[prop]);
-  }
-  for (let i = 0; i < entryValues.length; i++) {
-    if (entryValues[i] === '' || gradeEntry.score === undefined || gradeEntry.course === undefined || gradeEntry.name === undefined) {
+app.post('/api/grades', (req, res) => { // uh oh time to change this so it doesnt rely on order
+  const params = [req.body.name, req.body.course, req.body.score];
+  for (let i = 0; i < params.length; i++) {
+    if (params[i] === '' || req.body.score === undefined || req.body.course === undefined || req.body.name === undefined) {
       res.status(400).json({
         error: 'Please enter a valid grade entry'
       });
       return;
     }
   }
-  gradeEntry.score = Number(req.body.score);
+  req.body.score = Number(req.body.score);
   if (!Number.isInteger(req.body.score) || req.body.score < 0 || req.body.score > 100) {
     res.status(400).json({
       error: 'Please enter a number between 0 and 100 for the score'
@@ -53,7 +49,7 @@ app.post('/api/grades', (req, res) => {
     values ($1, $2, $3)
     returning *
   `;
-  db.query(sql, entryValues)
+  db.query(sql, params)
     .then(result => {
       res.status(201).json(result.rows[0]);
     })
@@ -66,15 +62,55 @@ app.post('/api/grades', (req, res) => {
 });
 
 app.put('/api/grades/:id', (req, res) => {
-  const gradeId = req.body.id;
-  const [,, x, y, z] = req.body;
+  const gradeId = Number(req.params.id);
+  const params = [req.body.name, req.body.course, req.body.score, gradeId];
+  if (!gradeId) {
+    res.status(400).send({
+      error: 'invalid gradeId, please enter an integer greater than 0'
+    });
+    return;
+  }
+  for (let i = 0; i < params.length; i++) {
+    if (params[i] === '' || req.body.score === undefined || req.body.course === undefined || req.body.name === undefined) {
+      res.status(400).json({
+        error: 'Please enter a valid grade entry'
+      });
+      return;
+    }
+  }
+  req.body.score = Number(req.body.score);
+  if (!Number.isInteger(req.body.score) || req.body.score < 0 || req.body.score > 100) {
+    res.status(400).json({
+      error: 'Please enter a number between 0 and 100 for the score'
+    });
+    return;
+  }
+
   const sql = `
   update "grades"
      set "name" = $1
          "course" = $2
          "score" = $3
-   where "
+   where "gradeId" = $4
+   returning *
   `;
+  db.query(sql, params)
+    .then(result => {
+      const queryResult = result.rows[0];
+      if (!queryResult) {
+        res.status(404).json({
+          error: `No entry with "gradeId" ${gradeId}`
+        });
+      } else {
+        res.status(200).json(result.rows[0]);
+      }
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).json({
+        error: 'an unexpected error occurred'
+      });
+    });
 });
 
 app.listen(3000, () => {
