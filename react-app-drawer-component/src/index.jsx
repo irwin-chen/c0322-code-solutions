42a737d6b50ecccc6a76339e03e1c1ecc62f32d2
Irwin Chen <irwin.t.chen@gmail.com>
2022-05-19 23:46:09 +0000

    Completed react-app-drawer-component

---
+++ react-app-drawer-component/src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Drawer from './app-drawer';
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(<Drawer/>);
