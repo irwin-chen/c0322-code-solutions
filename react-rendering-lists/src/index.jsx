984de4ea83a70e670c35c7c4c13b7c375a99d288
Irwin Chen <irwin.t.chen@gmail.com>
2022-05-20 17:30:20 +0000

    Completed react-rendering-lists

---
+++ react-rendering-lists/src/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const pokedex = [
+  { number: '001', name: 'Bulbasaur' },
+  { number: '004', name: 'Charmander' },
+  { number: '007', name: 'Squirtle' },
+  { number: '025', name: 'Pikachu' },
+  { number: '039', name: 'Jigglypuff' }
+];
+
+const listItems = pokedex.map(entry => <li key={entry.number}>{entry.name}</li>);
+
+const element = (
+  <ul>
+    {listItems}
+  </ul>
+);
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(element);
