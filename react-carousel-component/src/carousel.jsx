37adba29f56e66887867a67ed5a0dca40be835f1
Irwin Chen <irwin.t.chen@gmail.com>
2022-05-25 17:16:59 +0000

    added bullet container, in prog functionality

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 const list = [
   {
-    id: '001',
+    id: 0,
     url: 'https://i.insider.com/5bdc8cd3da27f86e7b74dc98?width=600&format=jpeg&auto=webp'
   },
   {
-    id: '002',
+    id: 1,
     url: 'https://www.gannett-cdn.com/presto/2021/06/16/USAT/005750ac-2e30-430d-ad8b-d1bce562cb77-NintendoSwitch_SuperSmashBrosUltimate_CharacterArt_06.png?crop=3384,4155,x615,y156'
   },
   {
-    id: '003',
+    id: 2,
     url: 'https://i.insider.com/5bdc869a110d4c6f5d30624a?width=1000&format=jpeg&auto=webp'
   },
   {
-    id: '004',
+    id: 3,
     url: 'https://i.insider.com/5bdc87eee4218a686f1f7901?width=1000&format=jpeg&auto=webp'
   },
   {
-    id: '005',
+    id: 4,
     url: 'https://www.pinclipart.com/picdir/middle/455-4552475_super-smash-bros-ultimate-characters-clipart.png'
   }
 ];
@@ -32,10 +32,19 @@ export default class Carousel extends React.Component {
     };
     this.nextImage = this.nextImage.bind(this);
     this.previousImage = this.previousImage.bind(this);
+    this.bulletClick = this.bulletClick.bind(this);
   }
 
   clicked() {
     clearInterval(timer);
+  }
+
+  bulletClick(event) {
+    this.clicked();
+    const selectedImage = this.state.image;
+    this.setState({
+      image: selectedImage
+    });
   }
 
   nextImage() {
@@ -52,7 +61,7 @@ export default class Carousel extends React.Component {
 
   previousImage() {
     this.clicked();
-    let prev = this.stage.image;
+    let prev = this.state.image;
     prev--;
     if (prev < 0) {
       prev = list.length - 1;
@@ -64,7 +73,7 @@ export default class Carousel extends React.Component {
 
   render() {
     timer = setInterval(this.nextImage, 3000);
-    const currentImageId = this.state.id;
+    const currentImageId = this.state.image;
     const images = list.map(entry => {
       if (entry.id === currentImageId) {
         return (
@@ -73,6 +82,17 @@ export default class Carousel extends React.Component {
       } else {
         return (
         <img className="image hidden" key={entry.id} imageid={entry.id} src={entry.url}></img>
+        );
+      }
+    });
+    const bulletList = list.map(entry => {
+      if (entry.id === currentImageId) {
+        return (
+          <i onClick={this.bulletClick} className="fa-solid fa-circle margin-side" key={entry.id}></i>
+        );
+      } else {
+        return (
+          <i onClick={this.bulletClick} className="fa-regular fa-circle margin-side" key={entry.id} id={entry.id}></i>
         );
       }
     });
@@ -85,6 +105,7 @@ export default class Carousel extends React.Component {
         </div>
         <div>
           <div className="bullet-container">
+            {bulletList}
           </div>
         </div>
       </div>
